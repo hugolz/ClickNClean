8f8e9e00fd82cf80a9503d17a8879756b1fdc2f8
hugolz <hugo.lezoualch@proton.me>
2024-03-12 16:57:43 +0100

    Cleaner profile update

--- clicknclean/src/main/java/model/CleanerExperience.java
+++ clicknclean/src/main/java/model/CleanerExperience.java
@@ -4,11 +4,11 @@ public enum CleanerExperience {
     NONE(1),
     LESS_1_YEARS(2),
     BETWEEN_1_3(3),
-	MORE_3_YEARS(4);
+    MORE_3_YEARS(4);
 
     private int experience;
 
-    CleanerExperience (int experience) {
+    CleanerExperience(int experience) {
         this.experience = experience;
     }
 
@@ -21,25 +21,38 @@ public enum CleanerExperience {
         case 3:
             return CleanerExperience.BETWEEN_1_3;
         case 4:
-        	return CleanerExperience.MORE_3_YEARS;
-        default: throw new Exception("Given experience doesn't match with cleaner experience" + experience);
+            return CleanerExperience.MORE_3_YEARS;
+        default:
+            throw new Exception("Given experience doesn't match with cleaner experience" + experience);
         }
     }
+
     public int asInt() {
         return this.experience;
     }
 
-    public static String asString(CleanerExperience cleanerExperience) throws Exception {
-        switch (cleanerExperience.asInt()) {
-            case 1:
-                return "Aucune expérience";
-            case 2:
-                return "Moins d'un an d'expérience en ménages";
-            case 3:
-                return "1 à 3 ans d'expérience";
-            case 4:
-                return "plus de 3 ans d'expérience";
-            default: throw new Exception("Given experience doesn't match with cleaner experience" + cleanerExperience);
-            }
+
+    // public static CleanerExperience fromString(String strExp) thows Exception{
+    //     switch (strExp) {
+    //     case "Aucune expérience":
+    //         return CleanerExperience.NONE;
+    //     case "Aucune expérience":
+    //     }
+
+    // }
+
+    public String toString() {
+        switch (this.experience) {
+        case 1:
+            return "Aucune expérience";
+        case 2:
+            return "Moins d'un an d'expérience en ménages";
+        case 3:
+            return "1 à 3 ans d'expérience";
+        case 4:
+            return "plus de 3 ans d'expérience";
+        default:
+            return "Given experience doesn't match with cleaner experience" + this.experience;
+        }
     }
 }
--- clicknclean/src/main/java/view/admin/AdminMain.java
+++ clicknclean/src/main/java/view/admin/AdminMain.java
@@ -30,58 +30,58 @@ import model.CleanerExperience;
 import tools.Db;
 
 public class AdminMain extends Scene {
-    public AdminMain(ScrollPane container, Window window, Admin admin) throws SQLException, Exception {
-        super(container, 800, 600);
-        System.out.println("AdminMain constructor");
-		
+	public AdminMain(ScrollPane container, Window window, Admin admin) throws SQLException, Exception {
+		super(container, 800, 600);
+		System.out.println("AdminMain constructor");
+
 		window.setTitle("Accueil d'Admin");
-		
-		Label welcomeMessageLabel = new Label("Bienvenue "+ admin.getSurname());
+
+		Label welcomeMessageLabel = new Label("Bienvenue " + admin.getSurname());
 		Label confirmCleanerLabel = new Label("Cleaners en attente de confirmation :");
 
-        ListView<String> listView = new ListView<String>();
+		ListView<String> listView = new ListView<String>();
 
-        ArrayList<Activity> activitiesList = refreshActivities(admin.getAdminId());
+		ArrayList<Activity> activitiesList = refreshActivities(admin.getAdminId());
 
-        for (Activity each : activitiesList) {
-            if (each.getType() == ActivityType.CLEANER_WAITING_TO_BE_CONFIRMED ) {
-                Db connection = new Db();
-                Cleaner currentCleaner = connection.DAOReadCleaner(each.getCleanerId());
+		for (Activity each : activitiesList) {
+			if (each.getType() == ActivityType.CLEANER_WAITING_TO_BE_CONFIRMED ) {
+				Db connection = new Db();
+				Cleaner currentCleaner = connection.DAOReadCleaner(each.getCleanerId());
 
 
-                ObservableList<String> items = FXCollections.observableArrayList();
-                items.add("Nom :" + currentCleaner.getSurname() + " " +currentCleaner.getName());
-                items.add("Date de naissance : " + currentCleaner.getBirthDate());
-                items.add("Expérience : " + CleanerExperience.asString(currentCleaner.getExperience()));
-                items.add("Motivation :  "+ currentCleaner.getMotivation());
-                listView.setItems(items);
-            }
-        }
-		
-        
+				ObservableList<String> items = FXCollections.observableArrayList();
+				items.add("Nom :" + currentCleaner.getSurname() + " " + currentCleaner.getName());
+				items.add("Date de naissance : " + currentCleaner.getBirthDate());
+				items.add("Expérience : " + currentCleaner.getExperience().toString());
+				items.add("Motivation :  " + currentCleaner.getMotivation());
+				listView.setItems(items);
+			}
+		}
+
+
 
 		Label displayDispute = new Label("Litiges à gérer");
 
-        ListView<String> listViewSuspend = new ListView<String>();
-		
+		ListView<String> listViewSuspend = new ListView<String>();
 
-        //for each...
-        listViewSuspend.getItems().add("Cleaner 1");
-        listViewSuspend.getItems().add("Cleaner 2");
-        listViewSuspend.getItems().add("Cleaner 3");
-		
 
-		
+		//for each...
+		listViewSuspend.getItems().add("Cleaner 1");
+		listViewSuspend.getItems().add("Cleaner 2");
+		listViewSuspend.getItems().add("Cleaner 3");
+
+
+
 		MenuBar bar = new MenuBar();
 		Menu disconnect = new Menu("Déconnexion");
 		Menu activities = new Menu("Notifications");
-		
-		
+
+
 
 		bar.getMenus().addAll(disconnect, activities);
-		
 
-		
+
+
 		EventHandler<ActionEvent> eventDisconnect = new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
 				//window.setScene(SceneId.CONNECTION);
@@ -90,45 +90,45 @@ public class AdminMain extends Scene {
 
 		};
 		disconnect.setOnAction(eventDisconnect);
-		
+
 		EventHandler<ActionEvent> eventViewProfil = new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
 				//go view Ownerprofil
 			}
 		};
-		
-		
+
+
 		VBox vbox = new VBox();
 		vbox.getChildren().add(bar);
 		vbox.getChildren().add(confirmCleanerLabel);
-        vbox.getChildren().add(listView);
+		vbox.getChildren().add(listView);
 		vbox.getChildren().add(displayDispute);
-        vbox.getChildren().add(listViewSuspend);
-		
-		
+		vbox.getChildren().add(listViewSuspend);
+
+
 		listView.setMaxHeight(100);
 		vbox.setSpacing(10);
 		vbox.setPadding(new Insets(100, 300, 20, 300));
 		vbox.setAlignment(Pos.TOP_CENTER);
 
 
-	    container.setContent(vbox);
+		container.setContent(vbox);
 
-	    container.setPannable(true);
-	    container.setVbarPolicy(ScrollBarPolicy.ALWAYS);
-	    container.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		container.setPannable(true);
+		container.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+		container.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
 
-            this.getStylesheets()
-        .add("file:///" + new File("src/main/css/style.css").getAbsolutePath().replace("\\", "/"));
+		this.getStylesheets()
+		.add("file:///" + new File("src/main/css/style.css").getAbsolutePath().replace("\\", "/"));
 	}
 
-    public ArrayList<Activity> refreshActivities(int adminId) throws SQLException, Exception {
-        Db connection = new Db();
-        ArrayList<Activity> refActivities = connection.DAOReadActivities(adminId);
-        connection.disconnect();
-        connection = null;
-        
-        return refActivities;
-    }
+	public ArrayList<Activity> refreshActivities(int adminId) throws SQLException, Exception {
+		Db connection = new Db();
+		ArrayList<Activity> refActivities = connection.DAOReadActivities(adminId);
+		connection.disconnect();
+		connection = null;
+
+		return refActivities;
+	}
 }
 
